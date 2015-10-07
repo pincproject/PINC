910fa15cf5f59351bd8d1a718926ac53d7934590
sigvaldm <sigvald.marholm@fys.uio.no>
2015-10-07 15:19:18 +0200

    Fått til å lese ini

--- DiP3D/src/main.c
+++ DiP3D/src/main.c
@@ -7,11 +7,30 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include "iniparser.h"
 
 int main(int argc, char *argv[]){
 
+	/**
+	 * READ INPUT FILE
+	 */
+
+	if(argc!=2){
+		fprintf(stderr,"Expects the input file as the only argument.\n");
+		return 1;
+	}
+
+	dictionary* ini;
+	ini = iniparser_load(argv[1]);
+
+	if(ini==NULL){
+		fprintf(stderr,"Could not parse %s.\n",argv[1]);
+		return 1;
+	}
+
 	for(int i=0;i<argc;i++){
-		printf("argv[%i]=%s\n",argv[i]);
+		printf("argv[%i]=%s\n",i,argv[i]);
 	}
 
 	return 0;
