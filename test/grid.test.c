a4bdd6019ffcc9a969b188a30f93edb1d1d0831b
sigvaldm <sigvald.marholm@fys.uio.no>
2015-12-17 13:49:59 +0100

    Added test.ini

--- test/grid.test.c
+++ test/grid.test.c
@@ -8,7 +8,36 @@
 
 #include "pinc.h"
 #include "test.h"
+#include "iniparser.h"
+
+dictionary *iniOpenDummy(){
+	return iniparser_load("test/test.ini");
+
+	dictionary *ini = malloc(sizeof(*ini));
+	iniparser_set(ini,"grid:trueSize","3,3,3");
+
+}
+
+int testGValDebug(){
+
+	dictionary *ini = iniOpenDummy();
+	Grid *grid = gAlloc(ini,3);
+	MpiInfo *mpiInfo = gAllocMpi(ini);
+	free(ini);
+
+	gValDebug(grid,mpiInfo);
+
+	// double *val = grid->val;
+
+	for(int i=0;i<60;i++){
+		// printf("%f\n",val[i]);
+	}
+
+	return 0;
+
+}
 
 // All tests for grid.c is contained in this function
 void testGrid(){
+	utRun(&testGValDebug);
 }
--- test/main.test.c
+++ test/main.test.c
@@ -7,15 +7,21 @@
  */
 
 #include "test.h"
+#include <mpi.h>
 
 int main(int argc, char *argv[]){
+
+	MPI_Init(&argc,&argv);
 
 	testAux();
 	testIo();
 	testGrid();
 	testPopulation();
 	testPusher();
+
 	utSummary();
+
+	MPI_Finalize();
 
 	return 0;
 }
