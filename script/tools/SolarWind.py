8b70551a3b8edb9f53efafae39c463bd7d88dcd6
trymen <trymen@math.uio.no>
2019-11-05 09:31:37 +0100

    Updated solar wind script with gyro frequency code, updated MMO ini files

--- script/tools/SolarWind.py
+++ script/tools/SolarWind.py
@@ -12,7 +12,7 @@ import sys
 import math as m
 import scipy.constants as c
     
-rad_dist = 0.607373#0.406821#
+rad_dist = float(sys.argv[1]) #0.406821#0.607373#
 a1d = 0.7766; a2d = -1.934; a3d = 0.01823; a4d = -2.245
 a1v = 2.651; a2v = 0.0; a3v = -0.0239; a4v = -1.836;
 a1t = 4.848; a2t = -0.668; a3t = -4.69e-42; a4t = -40.91;
@@ -23,17 +23,20 @@ Yv = a1v + a2v * X + a3v * m.exp(a4v * X)
 Yt = a1t + a2t * X + a3t * m.exp(a4t * X)
 #temp = (Yt**2 * c.electron_mass) / c.Boltzmann #in Kelvin
 
-num_density = m.pow(10,Yd) #density in cm^-3
+num_density = m.pow(10,Yd) * 1e6 #density in m^-3
 drift = m.pow(10,Yv) #drift velocity in km/s
 temp = m.pow(10,Yt) #temp in kelvin
 
 thermal_vel_e = m.sqrt((3 * c.Boltzmann * temp) / c.electron_mass) #in m/s
 thermal_vel_p = m.sqrt((3 * c.Boltzmann * temp) / c.proton_mass) #in m/s
 
-debye = m.sqrt((c.epsilon_0 * c.Boltzmann * temp) / (c.elementary_charge**2 * 4 * m.pi * num_density * 1e6))
-print "Numerical density of plasma at %f AU is %f cm^-3 \n" % (rad_dist, num_density)
-print "Drift velocity of plasma at %f is %f km/s \n" % (rad_dist, drift)
-print "Thermal velocity of plasma (electrons) at %f is %f km/s" % (rad_dist, thermal_vel_e/1000)
-print "Thermal velocity of plasma (protons) at %f is %f km/s" % (rad_dist, thermal_vel_p/1000)
+gyro_freq = m.sqrt((num_density * c.elementary_charge**2)/(c.electron_mass * c.epsilon_0)) #in Hertz
+debye = m.sqrt((c.epsilon_0 * c.Boltzmann * temp) / (c.elementary_charge**2 * 4 * m.pi * num_density))
+
+print "Numerical density of plasma at %f AU is %f m^-3 \n" % (rad_dist, num_density)
+print "Electron plasma frequency at %f AU is %f sec" % (rad_dist, 1./gyro_freq)
+print "Drift velocity of plasma at %f AU is %f km/s \n" % (rad_dist, drift)
+print "Thermal velocity of plasma (electrons) at %f AU is %f km/s" % (rad_dist, thermal_vel_e/1000)
+print "Thermal velocity of plasma (protons) at %f AU is %f km/s" % (rad_dist, thermal_vel_p/1000)
 print "Debye length of plasma at %f AU is %f meters \n" % (rad_dist, debye)
 print "Plasma temperature at %f AU is %f Kelvin \n" %(rad_dist, temp)
