d3985fc88b829b1faf117ad6be55ebb4363708c0
trymen <trymen@math.uio.no>
2019-11-13 13:58:16 +0100

    added matplotlib code to plot voxels for object

--- script/ConstructGrid/ConstructGrid.py
+++ script/ConstructGrid/ConstructGrid.py
@@ -7,9 +7,9 @@
 * @date                03.03.2016
 *
 * Todo:
-* - The findCircVoxels function is not entirely waterproof. When underresolving a tricky geometry 
-* using a non-uniform grid (i.e. when dx >> dy or dz) not all needed circumfering voxels are 
-* identified to make the floodfill successful. 
+* - The findCircVoxels function is not entirely waterproof. When underresolving a tricky geometry
+* using a non-uniform grid (i.e. when dx >> dy or dz) not all needed circumfering voxels are
+* identified to make the floodfill successful.
 * (test case: box.vtk, transfo[1] = [-1.5,-1.5,-1.5,45,45,45,0.5,0.5,0.5], gridpar = [-3,3,-3,3,-3,3,101,301,51])
 * - Implement a binary write function that PINC will like.
 * - Test the user input for sanity before continuing.
@@ -22,6 +22,8 @@ from math import *
 import numpy as np
 import time as time
 import vtk as vtk
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 #from matplotlib.pylab import * # only needed for testing purposes
 import src.GridTools as gg # import all tools to generate the grid
 content = [1, (0,0,0),1]*100 # initialise default
@@ -33,94 +35,101 @@ transfo = [0,0,0,0,0,0,1,1,1]*100 # initialise default
 # BEGIN USER INPUT #
 ####################
 
-# Define up the grid: xmin,xmax,ymin,ymax,zmin,zmax,nnx,nny,nnz
-gridpar = [-10, 10, -10, 10, -10, 10, 64, 64, 64] #0.3125
-#gridpar = [0, 0.3125*64, 0, 0.3125*64, 0, 0.3125*64, 64, 64, 64] #0.3125
-#gridpar = [0,0.19634*32,0,0.19634*32,0,0.19634*32,32,32,32]
+def main():
+    # Define up the grid: xmin,xmax,ymin,ymax,zmin,zmax,nnx,nny,nnz
+    gridpar = [-10, 10, -10, 10, -10, 10, 64, 64, 64] #0.3125
+    #gridpar = [0, 0.3125*64, 0, 0.3125*64, 0, 0.3125*64, 64, 64, 64] #0.3125
+    #gridpar = [0,0.19634*32,0,0.19634*32,0,0.19634*32,32,32,32]
 
-# List if object files. (VTK tetrahedralized unstructered grid, i.e., bunch of triangles)
-infile = ["mmo"]
-#infile = ["box", "box"]
-# Outputfiles and comment.
-outfile = ["object.grid.h5", "test satellite"]
-boundaryFile = ["bound.grid.h5", "bounding box"]
-# Object file contents. Provide one entry for each file in "infile".
-# [nr. of objects in file, tuple/coordinates of internal seed for each object before transformation (as much as needed), integer
-# identifier (one per file, assume all objects in file have the same floating potential) {-> Sigvald, we could use a predefined
-# number say "666" for insulators or dielectrics.} Note, the default is "0" for free voxels.]
-content[0] = [1, (0,0,0.5),1]
-#content[1] = [1, (0,0,0),2]
-#content[2] = [1, (0,0,0),3]
-#content[3] = [2, (0.1,0,0),(0.9,0,0),4]
+    # List if object files. (VTK tetrahedralized unstructered grid, i.e., bunch of triangles)
+    infile = ["box"]
+    #infile = ["box", "box"]
+    # Outputfiles and comment.
+    outfile = ["object.grid.h5", "test satellite"]
+    boundaryFile = ["bound.grid.h5", "bounding box"]
+    # Object file contents. Provide one entry for each file in "infile".
+    # [nr. of objects in file, tuple/coordinates of internal seed for each object before transformation (as much as needed), integer
+    # identifier (one per file, assume all objects in file have the same floating potential) {-> Sigvald, we could use a predefined
+    # number say "666" for insulators or dielectrics.} Note, the default is "0" for free voxels.]
+    content[0] = [1, (0,0,0.5),1]
+    #content[1] = [1, (0,0,0),2]
+    #content[2] = [1, (0,0,0),3]
+    #content[3] = [2, (0.1,0,0),(0.9,0,0),4]
 
-# Define the transformation. Provide one entry for each file in "infile".
-# Translate x,y,z; Rotate x,y,z (in degrees); Scale x,y,z
-transfo[0] = [0,0,0,0,0,0,1,1,1]
-#transfo[1] = [-1.5,-1.5,-1.5,45,45,45,0.5,0.5,0.5]
-#transfo[1] = [2.6,2.6,2.6,0,0,0,1,1,1]
-#transfo[3] = [0.75,0.75,-0.75,45,-45,-45,2,5,5]
+    # Define the transformation. Provide one entry for each file in "infile".
+    # Translate x,y,z; Rotate x,y,z (in degrees); Scale x,y,z
+    transfo[0] = [0,0,0,0,0,0,1,1,1]
+    #transfo[1] = [-1.5,-1.5,-1.5,45,45,45,0.5,0.5,0.5]
+    #transfo[1] = [2.6,2.6,2.6,0,0,0,1,1,1]
+    #transfo[3] = [0.75,0.75,-0.75,45,-45,-45,2,5,5]
 
-####################
-# END USER INPUT   #
-####################
+    ####################
+    # END USER INPUT   #
+    ####################
 
-# Now get out of this file and run it using python 2.x : $ python ConstructGrid.py  
+    # Now get out of this file and run it using python 2.x : $ python ConstructGrid.py
 
-######################################################################################################################################
-######################################################################################################################################
+    ######################################################################################################################################
+    ######################################################################################################################################
 
-print ("\n Welcome to ConstructGrid for PINC!\n")
-start = time.time()
+    print ("\n Welcome to ConstructGrid for PINC!\n")
+    start = time.time()
 
-# Partial sanity check (currently only checks whether you are overwriting input files).
-#No sanity checks here, Trym is insane 170619
-#gg.checkSanity(infile,outfile)
+    # Partial sanity check (currently only checks whether you are overwriting input files).
+    #No sanity checks here, Trym is insane 170619
+    #gg.checkSanity(infile,outfile)
 
-# Initialise the grid.
-grid = np.zeros((gridpar[6],gridpar[7],gridpar[8]))
+    # Initialise the grid.
+    grid = np.zeros((gridpar[6],gridpar[7],gridpar[8]))
 
-# Initialise a list to hold all tetrahedra. One element per object file.
-np_pts = [None]*len(infile)
+    # Initialise a list to hold all tetrahedra. One element per object file.
+    np_pts = [None]*len(infile)
 
-# Loop over all object files.
-for i in range(len(infile)):
-    ostart = time.time()
-    print (" Object:", infile[i])
-    # Read input files.
-    print( " 1. Reading.")
-    np_pts[i] = gg.readUnstructuredVTK(infile[i])
-    #Rotate/translate/scale.
-    print (" 2. Transform.")
-    np_pts[i] = gg.transformObject(np_pts[i], transfo[i])
-    print (" 3. Asign voxels.")
-    grid = gg.findCircVoxels(grid,gridpar,np_pts[i],content[i][-1])
-    # Fill the object.
-    # First transform the seeds.
-    content[i] = gg.transformSeeds(content[i], transfo[i])
-    # Now use floodfill.
-    grid = gg.floodFill(grid,gridpar,content[i])
-    # Compute bounding box of object
-    box = gg.boundingBox(grid)
-    print (box)
-    print(np.sum(box))
-    # Time info.
-    ostop = time.time()
-    print (" Compute time:", ostop-ostart, "sec.\n")
+    # Loop over all object files.
+    for i in range(len(infile)):
+        ostart = time.time()
+        print (" Object:", infile[i])
+        # Read input files.
+        print( " 1. Reading.")
+        np_pts[i] = gg.readUnstructuredVTK(infile[i])
+        #Rotate/translate/scale.
+        print (" 2. Transform.")
+        np_pts[i] = gg.transformObject(np_pts[i], transfo[i])
+        print (" 3. Asign voxels.")
+        grid = gg.findCircVoxels(grid,gridpar,np_pts[i],content[i][-1])
+        # Fill the object.
+        # First transform the seeds.
+        content[i] = gg.transformSeeds(content[i], transfo[i])
+        # Now use floodfill.
+        grid = gg.floodFill(grid,gridpar,content[i])
+        # Compute bounding box of object
+        box = gg.boundingBox(grid)
+        print(np.sum(box))
+        # Time info.
+        ostop = time.time()
+        print (" Compute time:", ostop-ostart, "sec.\n")
 
-# Write to file.
-wstart = time.time()
-print( " 4. Write output:")
-gg.writeOutput(grid, gridpar, outfile)
-gg.writeOutput(box, gridpar, boundaryFile)
-wstop = time.time()
-print (" Compute time:", wstop-wstart, "sec.\n")
+    # Write to file.
+    wstart = time.time()
+    print( " 4. Write output:")
+    gg.writeOutput(grid, gridpar, outfile)
+    gg.writeOutput(box, gridpar, boundaryFile)
+    wstop = time.time()
+    print (" Compute time:", wstop-wstart, "sec.\n")
 
-# Find total computing time.
-stop = time.time()
-print (" Total compute time:", stop-start, "sec.")
-    
-print ("\n All done, now go have a beer!\n")
+    # Find total computing time.
+    stop = time.time()
+    print (" Total compute time:", stop-start, "sec.")
+    print ("\n All done, now go have a beer!\n")
+    return grid
 
+if __name__ == '__main__':
+    grid = main()
+    grid_bool = np.array(grid, dtype=bool)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.voxels(grid_bool)
+    plt.show()
 ######################################################################################################################################
 #matshow(grid[:,:,101])
 #show()
