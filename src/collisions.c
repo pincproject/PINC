d6f305417fcb33a6dc240b8610aeedbac17982c2
steffen <steffen.brask@fys.uio.no>
2020-04-28 13:30:50 +0200

    updated line that checks convergence in multigrid to a semi-linear function

--- src/collisions.c
+++ src/collisions.c
@@ -2298,7 +2298,7 @@ static void oCollMode(dictionary *ini){
 		// Example of writing another dataset to history.xy.h5
 		// xyWrite(history,"/group/group/dataset",(double)n,value,MPI_SUM);
 
-		if(n%1000 == 0 || (n>9500 && n%10==0) ){//50614
+		if(n%1000 == 0 || (n>29500 && n%10==0) ){//50614
 		//Write h5 files
 		//gWriteH5(E, mpiInfo, (double) n);
 			gWriteH5(rho, mpiInfo, (double) n);
