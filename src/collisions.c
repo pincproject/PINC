93cab6d19d19967b38ab892b9d127ea73077c5c7
steffemb <steffemb@student.matnat.uio.no>
2020-09-18 15:37:22 +0200

    minor correction

--- src/collisions.c
+++ src/collisions.c
@@ -2402,7 +2402,7 @@ static void oCollMode(dictionary *ini){
 		// Example of writing another dataset to history.xy.h5
 		// xyWrite(history,"/group/group/dataset",(double)n,value,MPI_SUM);
 
-		if(n%10 == 0 || (n>24000 && n%100==0) ){//50614
+		if(n%1000 == 0 || (n>24000 && n%100==0) ){//50614
 		//Write h5 files
 		//gWriteH5(E, mpiInfo, (double) n);
 			gWriteH5(rho, mpiInfo, (double) n);
